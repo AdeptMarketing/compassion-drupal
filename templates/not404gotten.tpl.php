aac4dd14c28ff58b103ea3ed57cfe37a89cc2253
Phil Birnie <pbirnie@marketingadept.com>
2013-10-30 17:48:47 -0400

    Initial Commit

---
+++ templates/not404gotten.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * Theme file with embed code for the Notfound project.
+ */
+?>
+<iframe src="<?php print $url; ?>" width="100%" height="900" frameborder="0" scrolling="no"></iframe>
